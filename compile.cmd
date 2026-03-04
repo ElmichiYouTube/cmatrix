02dee81e779abce11d86903674c5b0ec4a5daef8
El_michiYT <juanbon3032@gmail.com>
2026-03-04 11:51:04 -0600

    Update compile.cmd

--- compile.cmd
+++ compile.cmd
@@ -7,8 +7,20 @@ chcp 65001 >nul
 :: Revisa si es Windows de 32 bits o de 64 bits
 IF "%PROCESSOR_ARCHITECTURE%"=="x64" (
     SET APP=C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe
+    SET APP2=C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe
 ) else (
     SET APP=C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe
+)
+
+:: Verifica si el usuario tiene el .NET Framework 4.0.30319
+if exist "%APP%" (
+    goto CONTINUAR
+) else (
+    echo [ERROR]: No tiene el Framework de .NET 4.0.30319
+    echo [ERROR]: No puede compilarlo con este script pero puede
+    echo intentar usar Visual Studio. O probar SharpDevelop.
+    echo (Es como Visual Studio pero sin consumirte como 4GB de RAM.
+    echo Y con una UI diferente y liviana.)
 )
 
 :: Mensaje de advertencia
@@ -28,6 +40,8 @@ IF ERRORLEVEL 1 (
     echo [NOTE]: Ahora puede cerrar el programa.
     pause >nul
 )
+if not exist "bin" mkdir "bin"
+if exist "bin\CMatrix.exe" del "bin\CMatrix.exe"
+move "CMatrix.exe" "bin"
 
-:: Seguridad
-exit
+goto :eof
